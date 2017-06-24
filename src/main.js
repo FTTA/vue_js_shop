cbb9869d11735df2c09882a9181fb2eb42509a41
riddleman <riddleman@ukr.net>
2017-06-25 01:09:06 +0300

    Moved main part of page to components

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import Itemtemplate from './Itemtemplate'
-import Recomendeditems from './Recomendeditems'
 
 import HeaderTop from './components/header/HeaderTop'
 import HeaderMiddle from './components/header/HeaderMiddle'
@@ -13,6 +11,8 @@ import FooterBottom from './components/footer/FooterBottom'
 import FooterTop from './components/footer/FooterTop'
 import FooterWidget from './components/footer/FooterWidget'
 
+import Content from './components/Content'
+
 import Slider from './Slider'
 // import router from './router'
 // var Itemtemplate = require('vue!./Itemtemplate.vue')
@@ -20,15 +20,6 @@ import Slider from './Slider'
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
-
-new Vue({
-  el: '#footer',
-  components: {
-    footertop: FooterTop,
-    footerwidget: FooterWidget,
-    footerbottom: FooterBottom
-  }
-})
 
 new Vue({
   el: '#header ',
@@ -47,36 +38,17 @@ new Vue({
 })
 
 new Vue({
-  el: '#vue_container_section',
-  // router,
-  // template: '<Itemtemplate/>',
+  el: '#content',
   components: {
-    Itemtemplate: Itemtemplate,
-    Recomendeditems: Recomendeditems
-  },
-  data: function () {
-    return {
-      goodsitems: [
-        {
-          'title': 'test goods',
-          'img': 'static/images/home/product1.jpg',
-          'id': '155',
-          'price': '10'
-        },
+    maincontent: Content
+  }
+})
 
-        {
-          'title': 'test goods',
-          'img': 'static/images/home/product1.jpg',
-          'id': '155',
-          'price': '10'
-        },
-        {
-          'title': 'test goods',
-          'img': 'static/images/home/product1.jpg',
-          'id': '155',
-          'price': '10'
-        }
-      ]
-    }
+new Vue({
+  el: '#footer',
+  components: {
+    footertop: FooterTop,
+    footerwidget: FooterWidget,
+    footerbottom: FooterBottom
   }
 })
